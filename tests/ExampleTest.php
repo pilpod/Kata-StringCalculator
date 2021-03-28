dce2e96d40011b01d510ed8a78fe7d08ed5f0eba
pilpod <giacomo.corso@gmail.com>
2021-03-28 16:10:27 +0200

    kata initialized

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase {
+
+    public function test_example()
+    {
+        $this->assertEquals(2, 2);
+    }
+
+}
+
+?>
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase {
+
+
+    
+
+
+}
+
+?>
